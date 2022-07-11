4330e6793270ee0a01f4ddde2b1afc5ab68b42b8
Esteban <estebanarivasv@gmail.com>
2022-07-11 19:59:32 -0300

    feat: add primereact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import Header from "./components/Header";
+import {NewSaleForm} from "./components/SaleForm";
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
 
 function App() {
   return (
     <div className="App">
+        {/* Body
+
         <Header />
-        {/* Body */}
+        */}
         <header className="bg-white shadow">
             <div className="max-w-7xl mx-auto py-3 px-4 sm:px-6 lg:px-8">
                 <h3 className="text-xl font-bold text-gray-600">Vendé tu auto</h3>
@@ -13,6 +20,7 @@ function App() {
         <main>
             <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                 {/* Replace with your content */}
+                <NewSaleForm />
                 <div className="px-4 py-4 sm:px-0">
                     <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"/>
                 </div>
