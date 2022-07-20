d2faa5e20f14f9bfa254be8e56069abbf9ca45e8
Esteban <estebanarivasv@gmail.com>
2022-07-20 19:27:53 -0300

    feat: add firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
-import firebase from "firebase/app";
+import {initializeApp} from "firebase/app";
 import {getAuth} from 'firebase/auth';
 
 // Initialize Firebase
-const app = firebase.initializeApp({
+const app = initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
@@ -15,4 +14,4 @@ const app = firebase.initializeApp({
 
 // Export auth module
 export const auth = getAuth(app);
-export default app
+export default app;
