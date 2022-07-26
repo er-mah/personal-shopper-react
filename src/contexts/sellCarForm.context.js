eadc228c947789d3b6a00cd55c31556561c56d20
Esteban <estebanarivasv@gmail.com>
2022-07-26 09:35:40 -0300

    refactor: edit forms to share

--- src/contexts/sellCarForm.context.js
+++ src/contexts/sellCarForm.context.js
@@ -8,10 +8,9 @@ export const SellCarProvider = ({children}) => {
 
     // Shared vars in context
     const [formData, setFormData] = useState({}); // State to store unsaved form data
-    const [proceedNext, setProceedNext] = useState(false); // Manage 'next' button within each component
 
     return (
-        <SellCarContext.Provider value={[formData, setFormData, proceedNext, setProceedNext]}>
+        <SellCarContext.Provider value={[formData, setFormData]}>
             {children}
         </SellCarContext.Provider>
     );
