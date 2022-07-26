9cbfe91a5f65c3febfaf655c154bf2b573e6e7b7
Esteban <estebanarivasv@gmail.com>
2022-07-26 09:32:38 -0300

    feat: add apps routes constants

---
+++ src/utils/constants.js
@@ -0,0 +1,24 @@
+export const MAIN_URLS = {
+    HOME: "/",
+    SIGN_IN: "/login",
+    SIGN_UP: "/register",
+    NEW_SALE_FORM: "/cotizacion"
+}
+
+export const NEW_SALE_FORM_URLS = {
+    BRAND: "/marca",
+    YEAR: "/antiguedad",
+    MODEL: "/modelo",
+    VEHICLE_DETAILS: "/detalles-del-vehiculo",
+    SIGN_IN: "/login",
+    OWNER: "/datos-propietario",
+    VEHICLE_COLOUR: "/color",
+    VEHICLE_STATE: "/estado-del-vehiculo",
+    LICENCE_PLATE: "/licencia",
+    SALE: "/requerimientos-venta",
+    QUOTATION: "/precio-estimado",
+    REVISION: "/inspeccion",
+    DONE: "/terminando"
+}
+
+export const MAH_RED_COLOUR = "#E21F25"
