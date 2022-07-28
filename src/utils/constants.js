72334c639774cae44350442a4574d24c633c78cd
Esteban <estebanarivasv@gmail.com>
2022-07-28 17:09:18 -0300

    refactor: delete unused form slides

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,14 +7,9 @@ export const MAIN_URLS = {
 
 export const NEW_SALE_FORM_URLS = {
     BRAND: "/marca",
-    YEAR: "/antiguedad",
-    MODEL: "/modelo",
     VEHICLE_DETAILS: "/detalles-del-vehiculo",
     SIGN_IN: "/login",
-    OWNER: "/datos-propietario",
     VEHICLE_COLOUR: "/color",
-    VEHICLE_STATE: "/estado-del-vehiculo",
-    LICENCE_PLATE: "/licencia",
     SALE: "/requerimientos-venta",
     QUOTATION: "/precio-estimado",
     REVISION: "/inspeccion",
