9707242d83848cb882951df1801f3784700cebcc
Esteban <estebanarivasv@gmail.com>
2022-07-28 17:04:50 -0300

    feat: add initial values to context

--- src/contexts/sellCarForm.context.js
+++ src/contexts/sellCarForm.context.js
@@ -7,7 +7,12 @@ export const SellCarContext = createContext({});
 export const SellCarProvider = ({children}) => {
 
     // Shared vars in context
-    const [formData, setFormData] = useState({}); // State to store unsaved form data
+    const [formData, setFormData] = useState({
+        vehicleBrandID: null,
+        vehicleYear: null,
+        vehicleModelID: null,
+        vehicleVersionCodiaID: null
+    }); // State to store unsaved form data
 
     return (
         <SellCarContext.Provider value={[formData, setFormData]}>
