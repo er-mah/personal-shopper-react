35ec946f315cd1104c7393edabc511ff0d6dbab0
Esteban <estebanarivasv@gmail.com>
2022-08-02 17:36:40 -0300

    feat: add quotation fetching

--- src/components/sale-form-steps/quotation.jsx
+++ src/components/sale-form-steps/quotation.jsx
@@ -1,67 +1,92 @@
-import {useNavigate} from "react-router-dom";
-import {MAIN_URLS, NEW_SALE_FORM_URLS} from "../../utils/constants";
-import {Button} from "primereact/button";
-import {Dropdown} from "primereact/dropdown";
-import {InputText} from "primereact/inputtext";
-import {useEffect} from "react";
+import { useNavigate } from "react-router-dom";
+import { MAIN_URLS, NEW_SALE_FORM_URLS } from "../../utils/constants";
+import { Button } from "primereact/button";
+import {useContext, useEffect, useState} from "react";
+import { SellCarContext } from "../../contexts";
+import VehicleService from "../../services/vehicle.service";
 
-export function Quotation({step, setStep}) {
+export function Quotation({ step, setStep }) {
+  let service = new VehicleService();
 
-    let navigate = useNavigate();
+  const [formData, setFormData, quotationInfo, setQuotationInfo] = useContext(SellCarContext);
+  const [minValue, setMinValue] = useState(null);
+  const [maxValue, setMaxValue] = useState(null);
 
-    const previousPage = () => {
-        navigate(MAIN_URLS.NEW_SALE_FORM + NEW_SALE_FORM_URLS.SALE)
-    }
+  let navigate = useNavigate();
 
-    const nextPage = () => {
-        navigate(MAIN_URLS.NEW_SALE_FORM + NEW_SALE_FORM_URLS.REVISION)
-    }
+  const previousPage = () => {
+    navigate(MAIN_URLS.NEW_SALE_FORM + NEW_SALE_FORM_URLS.VEHICLE_DETAILS);
+  };
 
-    // When component is rendered
-    useEffect(() => {
-        setStep(6); // Set progress bar status
-    }, []);
+  const nextPage = () => {
+    navigate(MAIN_URLS.NEW_SALE_FORM + NEW_SALE_FORM_URLS.REVISION);
+  };
 
-    return (
-        <>
-            <div className="grid">
-                <div className="col-1 flex justify-content-center">
-                    <Button icon="pi pi-angle-left"
-                            className="p-button-rounded p-button-text p-button-danger"
-                            aria-label="Back"
-                            onClick={() => previousPage()}/>
-                </div>
-                <div className="col-10">
-                    <h2>Conocé la cotización de tu vehículo</h2>
-                    <p>Si lo vendes con nosotros, recibís:</p>
-                    <div className="card">
-                        <div className="flex flex-wrap align-items-center justify-content-center card-container">
-                            <div className="surface-overlay border-round border-1 p-3 m-3 w-16rem min-h-full">
-                                $ 900.000 - $ 1.200.000
-                            </div>
-                        </div>
-                    </div>
-                    <p>Otras agencias te lo pueden recibir a:</p>
-                    <div className="card">
-                        <div className="flex flex-wrap align-items-center justify-content-center card-container">
-                            <div className="surface-overlay border-round border-1 p-3 m-3 w-16rem min-h-full">
-                                $ 700.000
-                            </div>
-                            <br/>
-                            <div className="surface-overlay border-round border-1 p-3 m-3 w-16rem min-h-full">
-                                $ 650.000
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-1 flex justify-content-center">
-                    <Button icon="pi pi-angle-right"
-                            className="p-button-rounded p-button-text p-button-danger"
-                            aria-label="Next"
-                            onClick={() => nextPage()}/>
-                </div>
-            </div>
+  // When component is rendered
+  useEffect(() => {
+    setStep(5); // Set progress bar status
 
-        </>
+    const payload = {
+      colour: quotationInfo.colorCategory,
+      kilometers: quotationInfo.kmsCategory,
+      sellingTime: quotationInfo.urgency,
+      state: quotationInfo.stateCategory,
+      versionId: quotationInfo.codia,
+      year: quotationInfo.year,
+    };
+
+    service.getQuotation(payload).then(
+        (res) => {
+          setMinValue(res.data.data.minValue)
+          setMaxValue(res.data.data.maxValue)
+        }
     );
+  }, []);
+
+  return (
+    <>
+      <div className="grid">
+        <div className="col-1 flex justify-content-center">
+          <Button
+            icon="pi pi-angle-left"
+            className="p-button-rounded p-button-text p-button-danger"
+            aria-label="Back"
+            onClick={() => previousPage()}
+          />
+        </div>
+        <div className="col-10">
+          {JSON.stringify(quotationInfo)}
+          <h2>Conocé la cotización de tu vehículo</h2>
+          <p>Si lo vendes con nosotros, recibís:</p>
+          <div className="card">
+            <div className="flex flex-wrap align-items-center justify-content-center card-container">
+              <div className="surface-overlay border-round border-1 p-3 m-3 w-16rem min-h-full">
+                $ {minValue} - $ {maxValue}
+              </div>
+            </div>
+          </div>
+          <p>Otras agencias te lo pueden recibir a:</p>
+          <div className="card">
+            <div className="flex flex-wrap align-items-center justify-content-center card-container">
+              <div className="surface-overlay border-round border-1 p-3 m-3 w-16rem min-h-full">
+                $ 700.000
+              </div>
+              <br />
+              <div className="surface-overlay border-round border-1 p-3 m-3 w-16rem min-h-full">
+                $ 650.000
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-1 flex justify-content-center">
+          <Button
+            icon="pi pi-angle-right"
+            className="p-button-rounded p-button-text p-button-danger"
+            aria-label="Next"
+            onClick={() => nextPage()}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
