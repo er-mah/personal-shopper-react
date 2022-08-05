7b3c27683707f9affe935e95712dc76b88c29917
Esteban <estebanarivasv@gmail.com>
2022-08-05 17:42:14 -0300

    refactor: delete default values

--- src/contexts/sellCarForm.context.js
+++ src/contexts/sellCarForm.context.js
@@ -1,50 +1,37 @@
-import {createContext, useState} from 'react';
+import { createContext, useState } from "react";
 
 // Context
 export const SellCarContext = createContext({});
 
 // Provider
-export const SellCarProvider = ({children}) => {
+export const SellCarProvider = ({ children }) => {
 
-    // Shared vars in context
-    const [formData, setFormData] = useState({
-        //brand: null,
-        year: null,
-        //model: null,
-        version: null,
-        versionId: null,
-        //codia: null,
-        //comfort: null,
-        //technicalInfo: null,
-        //engineAndTransmission: null,
-        //security: null,
-        //color: null,
-        //kilometers: null,
-        //licensePlateType: null,
-        //licensePlate: null,
-        //state: null,
-        //comments: null,
-        //currency: null,
-        //requiredAmount: null,
-        //urgency: null,
-        //priceRange: null,
-        //revisionDatetime: null
-    }); // State to store unsaved form data
+  // Store unsaved form data
+  const [formData, setFormData] = useState({});
+  // FORM VARIABLES
+  // brandName  brandId         year  modelId  modelName  versionName   versionId
+  // comfort    technicalInfo   engineAndTransmission     security
+  // colour     kilometers      licensePlateType          licensePlate
+  // state      comments        currency                  requiredAmount
+  // urgency    priceRange      revisionDatetime
 
-    const [quotationInfo, setQuotationInfo] = useState({
-        year: null,
-        codia: null,
-        colorCategory: null,
-        stateCategory: null,
-        urgencyCategory: null,
-        kmsCategory: null
-    });
+  // Store data to make requests to get the vehicle quotation
+  const [quotationInfo, setQuotationInfo] = useState({});
+  // QUOTATION VARIABLES
+  // year             codia               colorCategory
+  // stateCategory    urgencyCategory     kmsCategory
 
-    const sharedVariables = [formData, setFormData, quotationInfo, setQuotationInfo]
+  // Shared vars in context
+  const sharedVariables = [
+    formData,
+    setFormData,
+    quotationInfo,
+    setQuotationInfo,
+  ];
 
-    return (
-        <SellCarContext.Provider value={sharedVariables}>
-            {children}
-        </SellCarContext.Provider>
-    );
+  return (
+    <SellCarContext.Provider value={sharedVariables}>
+      {children}
+    </SellCarContext.Provider>
+  );
 };
