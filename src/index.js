0824fb406c6bef66247324e187cab8bf01efe0fd
Esteban <estebanarivasv@gmail.com>
2022-08-16 19:59:15 -0300

    refactor: optimize constants

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";  // theme
 import "primereact/resources/primereact.min.css";                  // core css
 import "primeicons/primeicons.css";                                // icons
 import "primeflex/primeflex.css";                                  // primeflex css
+import "primeflex/primeflex.min.css";                                  // primeflex css
 import './utils/styles/index.css';
 
 import React from 'react';
