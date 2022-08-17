a36a90f640cd8d135b81985a0d07dbe3d2ae2692
Esteban <estebanarivasv@gmail.com>
2022-08-16 23:06:15 -0300

    feat: add minor change

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import { Header } from "./header";
 import { NewSaleForm } from "./newSaleForm";
-import { MAIN_URLS } from "../utils/constants";
+import { MAIN_URLS, NEW_SALE_FORM_URLS } from "../utils/constants";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
+        <Route
+          path={""}
+          element={
+            <Navigate
+              to={MAIN_URLS.NEW_SALE_FORM + NEW_SALE_FORM_URLS.START}
+              replace
+            />
+          }
+        />
         <Route
           path={MAIN_URLS.NEW_SALE_FORM + "/*"}
           element={<NewSaleForm />}
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-import "primereact/resources/themes/lara-light-indigo/theme.css";  // theme
-import "primereact/resources/primereact.min.css";                  // core css
-import "primeicons/primeicons.css";                                // icons
-import "primeflex/primeflex.css";                                  // primeflex css
-import "primeflex/primeflex.min.css";                                  // primeflex css
-import './utils/styles/index.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
+import "primereact/resources/primereact.min.css"; // core css
+import "primeicons/primeicons.css"; // icons
+import "primeflex/primeflex.css"; // primeflex css
+import "primeflex/primeflex.min.css"; // primeflex css
+import "./utils/styles/index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './components/app';
+import App from "./components/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
