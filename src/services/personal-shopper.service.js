7f4563dc3a308a8fa2c9d254dd0d6f38fa5e13bf
Esteban <estebanarivasv@gmail.com>
2022-09-15 23:40:33 -0300

    feat: change business model logic

--- src/services/personal-shopper.service.js
+++ src/services/personal-shopper.service.js
@@ -2,9 +2,25 @@ import axios from "axios";
 
 function PersonalShopperService() {
 
-  this.persistForm = async (formData) => {
+  this.persistDeal = async (formData) => {
     try {
-      return axios.post(`${process.env.REACT_APP_MAH_URI}/personal-shopper/persist-form`, formData);
+      return axios.post(`${process.env.REACT_APP_MAH_URI}/personal-shopper/persist-deal`, formData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  this.addRevisionDatesToDeal = async (revisionDatesPayload, dealId) => {
+    try {
+      return axios.post(`${process.env.REACT_APP_MAH_URI}/personal-shopper/${dealId}/revision-dates`, revisionDatesPayload);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  this.addSaleTypeToDeal = async (saleTypePayload, dealId) => {
+    try {
+      return axios.post(`${process.env.REACT_APP_MAH_URI}/personal-shopper/${dealId}/sale-type`, saleTypePayload);
     } catch (err) {
       console.error(err);
     }
